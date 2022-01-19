0423296e2e95b878d33dd5747c2ca46f48f703a2
Dominik Vít <mail@dominikvit.cz>
2022-01-19 23:43:34 +0100

    Fixed ts warnings

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import EndScreen from "./screens/EndScreen";
 import GameScreen from "./screens/GameScreen";
 import StartScreen from "./screens/StartScreen";
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { currentSubjectState, questionsState } from "./state/questionsState";
 import { useRecoilState } from "recoil";
 import { Response } from "./core/types";
@@ -10,29 +10,36 @@ import ErrorCard from "./components/ErrorCard";
 
 function App() {
 
-  const [_, setCurrentSubject] = useRecoilState(currentSubjectState)
-  const [__, setQuestions] = useRecoilState(questionsState);
+  const [currentSubject, setCurrentSubject] = useRecoilState(currentSubjectState)
+  const [questions, setQuestions] = useRecoilState(questionsState);
   const [questionsLoading, setQuestionsLoading] = useState<boolean>(false);
   const [questionsLoadingError, setQuestionsLoadingError] = useState<boolean>(false);
 
-  const loadQuestions = async () => {
-    setQuestionsLoading(true);
-    try {
-      const quesetionsResponse = await fetch('/questions.json');
-      const fetchedQuestions: Response = await quesetionsResponse.json();
-      const newSubject = fetchedQuestions.subjects[Math.round(Math.random() * (fetchedQuestions.subjects.length - 1))];
-      setCurrentSubject(newSubject);
-      setQuestions(fetchedQuestions.questions[newSubject.id]);
-    } catch (e) {
-      setQuestionsLoadingError(true);
+
+
+  const loadQuestions = useCallback(() => {
+    async function init() {
+      setQuestionsLoading(true);
+      try {
+        const quesetionsResponse = await fetch('/questions.json');
+        const fetchedQuestions: Response = await quesetionsResponse.json();
+        const newSubject = fetchedQuestions.subjects[Math.round(Math.random() * (fetchedQuestions.subjects.length - 1))];
+        setCurrentSubject(newSubject);
+        setQuestions(fetchedQuestions.questions[newSubject.id]);
+      } catch (e) {
+        setQuestionsLoadingError(true);
+      }
+      setQuestionsLoading(false);
     }
-    setQuestionsLoading(false);
-  }
+    init();
+  }, [setCurrentSubject, setQuestions, setQuestionsLoading, setQuestionsLoadingError]);
 
   useEffect(() => {
     loadQuestions();
-  }, [])
-
+  }, [loadQuestions])
+  
+  console.log(currentSubject);
+  console.log(questions);
   if (questionsLoading) {
     return (<div></div>)
   }
